995ad105316c588791bcf3b5770152d4c1f9334b
Megs <Megs.close@gmail.com>
2021-03-01 13:15:53 -0800

    thing of arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,41 @@
 // adding the current day
 $("#currentDay").text(moment().format("ddd MMMM do, YYYY"));
 
+var hours = [
+    {
+        hour: 9
+
+    },
+    {
+        hour: 10
+
+    },
+    {
+        hour: 11
+
+    },
+    {
+        hour: 12
+
+    },    
+    {
+        hour: 1
+
+    },    
+    {
+        hour: 2
+
+    },    
+    {
+        hour: 3
+
+    },
+    {
+        hour: 4
+
+    },
+    {
+        hour: 5
+
+    }
+]
