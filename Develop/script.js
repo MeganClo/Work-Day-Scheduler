59e2417e551576426455643d23e358d52a2217e9
Megs <Megs.close@gmail.com>
2021-03-04 17:39:46 -0800

    trying to get ids into a variable to compare

--- Develop/script.js
+++ Develop/script.js
@@ -4,62 +4,24 @@ $("#currentDay").text(moment().format("ddd MMMM do, YYYY"));
 //getting current hour 
 moment().format('hh');
 
-var blocks = [
-    {
-        hour: "9AM",
-        description: "",
-        time: "09"
-    },
-    {
-        hour: "10AM",
-        description: "",
-        time: "10"
-    },
-    {
-        hour: "11AM",
-        description: "",
-        time: "11"
-    },
-    {
-        hour: "12PM",
-        description: "",
-        time: "12"
-    },    
-    {
-        hour: "1PM",
-        description: "",
-        time: "01"
-    },    
-    {
-        hour: "2PM",
-        description: "",
-        time: "02"
-    },    
-    {
-        hour: "3PM",
-        description: "",
-        time: "03"
-    },
-    {
-        hour: "4PM",
-        description: "",
-        time: "04"
-    },
-    {
-        hour: "5PM",
-        description: "",
-        time: "05"
-    }
-];
+$(".time-block").each(function() {
+    var hourBlock = $(this).attr("id");
+    console.log(hourBlock)
+});
 
 //saves data to local storage
-function saveDescriptions() {
-    localStorage.setItem("blocks", JSON.stringify(blocks));
-};
+//function saveDescriptions() {
+//    localStorage.setItem("blocks", JSON.stringify(blocks));
+//};
 
-var newBlocks = JSON.parse(localStorage.getItem("blocks"));
+//var newBlocks = JSON.parse(localStorage.getItem("blocks"));
 
-console.log(newBlocks);
+//console.log(newBlocks);
+
+//newBlocks.forEach(function(rows) {
+//    $(".container").append("<div class='row time-block'></div>");
+//    $(".row").append()
+//})
 
 $(".container").append("<div class='row time-block'></div>");
 //    $(".container").append("<div class='row time-block'></div>");
